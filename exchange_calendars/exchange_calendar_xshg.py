26dbb3669b9e375e03ec437473acb1b2a1e64f9c
piretoo <polarisnorth@inbox.lv>
2025-12-25 13:53:49 +0800

    XSHG: Add 2026 holidays

--- exchange_calendars/exchange_calendar_xshg.py
+++ exchange_calendars/exchange_calendar_xshg.py
@@ -593,6 +593,46 @@ precomputed_shanghai_holidays = pd.to_datetime(
         "2025-10-06",
         "2025-10-07",
         "2025-10-08",
+        # 2026 holidays - Source: Shanghai Stock Exchange notice dated 2025-12-22 https://www.sse.com.cn/disclosure/announcement/general/c/c_20251222_10802507.shtml
+        "2026-01-01",
+        "2026-01-02",
+        "2026-01-03",
+        "2026-01-04",
+        "2026-02-14",
+        "2026-02-15",
+        "2026-02-16",
+        "2026-02-17",
+        "2026-02-18",
+        "2026-02-19",
+        "2026-02-20",
+        "2026-02-21",
+        "2026-02-22",
+        "2026-02-23",
+        "2026-02-28",
+        "2026-04-04",
+        "2026-04-05",
+        "2026-04-06",
+        "2026-05-01",
+        "2026-05-02",
+        "2026-05-03",
+        "2026-05-04",
+        "2026-05-05",
+        "2026-05-09",
+        "2026-06-19",
+        "2026-06-20",
+        "2026-06-21",
+        "2026-09-20",
+        "2026-09-25",
+        "2026-09-26",
+        "2026-09-27",
+        "2026-10-01",
+        "2026-10-02",
+        "2026-10-03",
+        "2026-10-04",
+        "2026-10-05",
+        "2026-10-06",
+        "2026-10-07",
+        "2026-10-10",
     ]
 )
 
@@ -607,8 +647,11 @@ class XSHGExchangeCalendar(PrecomputedExchangeCalendar):
     Close time: 15:00 Asia/Shanghai
 
     Due to the complexity around the Shanghai exchange holidays, we are
-    hardcoding a list of holidays covering 1999-2025, inclusive. There are
+    hardcoding a list of holidays covering 1999-2026, inclusive. There are
     no known early closes or late opens.
+
+    2026 holidays source: Shanghai Stock Exchange notice dated 2025-12-22.
+    https://www.sse.com.cn/disclosure/announcement/general/c/c_20251222_10802507.shtml
     """
 
     name = "XSHG"
